bafd4a5d0e90ee0c37020c117b65901a922c6939
Tomek Wójcik <labs@tomekwojcik.pl>
2012-11-24 22:30:21 +0100

    Initial Commit

---
+++ tune-control/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  tune-control
+//
+//  Created by Tomek Wójcik on 11/24/12.
+//  Copyright (c) 2012 BTHLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
